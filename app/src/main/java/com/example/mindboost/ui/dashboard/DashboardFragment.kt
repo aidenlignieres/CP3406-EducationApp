e18c0b681e243de87b923a4f674b9155efcec32e
Aiden Lignieres <46545209+aidenlignieres@users.noreply.github.com>
2023-05-21 11:57:38 +1000

    Fixed bottom nav bar

--- app/src/main/java/com/example/mindboost/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/mindboost/ui/dashboard/DashboardFragment.kt
@@ -28,7 +28,7 @@ class DashboardFragment : Fragment() {
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.soundSwitch
+        val textView: TextView = binding.settingsTitle
         dashboardViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
