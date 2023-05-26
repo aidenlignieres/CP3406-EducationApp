0a72817de047c63e224d8fc762e9c3e56079279d
Aiden Lignieres <46545209+aidenlignieres@users.noreply.github.com>
2023-05-26 22:57:23 +1000

    Added button listeners and function to call for questions class

--- app/src/main/java/com/example/mindboost/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/mindboost/ui/home/HomeFragment.kt
@@ -1,17 +1,25 @@
+@file:Suppress("UNREACHABLE_CODE")
+
 package com.example.mindboost.ui.home
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.mindboost.QuestionActivity
 import com.example.mindboost.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
+    private lateinit var mathButton: Button
+    private lateinit var scienceButton: Button
+    private lateinit var readingButton: Button
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -23,7 +31,7 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+            ViewModelProvider(this)[HomeViewModel::class.java]
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
@@ -32,7 +40,34 @@ class HomeFragment : Fragment() {
         homeViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+
+        mathButton = binding.mathButton
+        scienceButton = binding.scienceButton
+        readingButton = binding.readingButton
+
+        // Button listeners
+        mathButton.setOnClickListener {
+            startQuestionActivity("Math")
+        }
+
+        scienceButton.setOnClickListener {
+            startQuestionActivity("Science")
+        }
+
+        readingButton.setOnClickListener {
+            startQuestionActivity("Reading")
+        }
+
         return root
+    }
+
+    // Activity starter
+    private fun startQuestionActivity(subject: String) {
+        val intent = Intent(requireActivity(), QuestionActivity::class.java)
+        intent.putExtra("subject", subject)
+        // println(subject)
+        // println(intent)
+        startActivity(intent)
     }
 
     override fun onDestroyView() {
