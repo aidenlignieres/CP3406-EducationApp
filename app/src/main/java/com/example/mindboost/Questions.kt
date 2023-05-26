caa4ee0c2b3e067d4a3687cb41b77b8a195e62e5
Aiden Lignieres <46545209+aidenlignieres@users.noreply.github.com>
2023-05-26 22:54:51 +1000

    Added functions to clear MutableSets, and adding questions to list.

--- app/src/main/java/com/example/mindboost/Questions.kt
+++ app/src/main/java/com/example/mindboost/Questions.kt
@@ -10,7 +10,6 @@ data class Question(
 
 object QuestionBank {
     private val allQuestions: List<Question> = listOf(
-        // place holder
         Question(
             1,
             "Math",
@@ -18,20 +17,60 @@ object QuestionBank {
             listOf("3.14", "2.71", "1.618", "0.5"),
             0
         ),
-        // place holder
         Question(
             2,
+            "Math",
+            "What is the remainder of 21 divided by 7?",
+            listOf("21", "7", "1", "None of Above"),
+            3
+        ),
+        Question(
+            3,
+            "Math",
+            "19 + ……. = 42",
+            listOf("23", "61", "0", "42"),
+            1
+        ),
+        Question(
+            4,
             "Science",
             "What is the chemical symbol for water?",
             listOf("O2", "H2O2", "CO2", "H2O"),
             3
         ),
-        // place holders
         Question(
-            3,
+            5,
+            "Science",
+            "What is the S.I unit of frequency?",
+            listOf("Doipter", "Second", "Hertz", "Meter"),
+            2
+        ),
+        Question(
+            6,
+            "Science",
+            "What is the PH range of acids?",
+            listOf("0-7", "7-14", "1-7", "7-15"),
+            3
+        ),
+        Question(
+            7,
             "Reading",
             "Who is the author of 'To Kill a Mockingbird'?",
             listOf("Harper Lee", "J.K. Rowling", "George Orwell", "Ernest Hemingway"),
+            0
+        ),
+        Question(
+            8,
+            "Reading",
+            "What’s the name of the spiky-headed Sith Lord holding a cool double-blade lightsaber?",
+            listOf("Darth Vader", "Darth Maul", "Darth Paul", "Darth Garth"),
+            0
+        ),
+        Question(
+            9,
+            "Reading",
+            "What spell did Harry use to kill Lord Voldemort?",
+            listOf("Expelliarmus", "Expecto Patronum", "Avada Kedavra", "Accio"),
             0
         )
         // Add more questions for different subjects
@@ -51,7 +90,7 @@ object QuestionBank {
         }
     }
 
-    fun resetUsedQuestions() {
+    fun resetQuestions() {
         usedQuestions.clear()
     }
 }
