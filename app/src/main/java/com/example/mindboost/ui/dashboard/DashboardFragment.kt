fe6c6b6e4e3a668ef14c369c077421f7a0a3843c
Aiden Lignieres <46545209+aidenlignieres@users.noreply.github.com>
2023-05-28 16:29:09 +1000

    Fixed the options for difficulty

--- app/src/main/java/com/example/mindboost/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/mindboost/ui/dashboard/DashboardFragment.kt
@@ -4,14 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.mindboost.R
 import com.example.mindboost.databinding.FragmentDashboardBinding
 
 class DashboardFragment : Fragment() {
 
     private var _binding: FragmentDashboardBinding? = null
+    private lateinit var selectedDifficulty: String
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -23,7 +27,7 @@ class DashboardFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+            ViewModelProvider(this)[DashboardViewModel::class.java]
 
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
@@ -32,11 +36,23 @@ class DashboardFragment : Fragment() {
         dashboardViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+
+        val difficultyRadioGroup: RadioGroup = root.findViewById(R.id.difficultyRadioGroup)
+
+        difficultyRadioGroup.setOnCheckedChangeListener { group, checkedId ->
+            val selectedRadioButton: RadioButton = group.findViewById(checkedId)
+            selectedDifficulty = selectedRadioButton.text.toString()
+        }
+
         return root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    fun getSelectedDifficulty(): String {
+        return selectedDifficulty
     }
 }
